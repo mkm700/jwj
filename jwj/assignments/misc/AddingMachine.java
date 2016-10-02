212ff6ca9be02a6ae00b81495dbdd1c1b63116cc
Mary Kay Murdaugh <murdaughmk@yahoo.com>
2016-10-02 14:27:33 -0500

    add misc assignments

---
+++ jwj/assignments/misc/AddingMachine.java
@@ -0,0 +1,34 @@
+package misc;
+
+import static java.lang.System.out; 
+import java.util.Scanner;
+
+public class AddingMachine
+{
+   
+    public static void main(String[] args) {
+        // initialise instance variables
+        Scanner keyboard = new Scanner(System.in);
+        int tally = 0;
+        float total = 0;
+        
+        out.println("Welcome to Adding Machine!");
+        out.println("Total: " + total + "\n");
+        out.print("Add: ");
+        float addNum = keyboard.nextFloat();
+        
+        while (addNum != 0) {
+            total = total + addNum;
+            tally++;
+            out.println("Total: " + total + "\n");
+            out.print("Add: ");
+            addNum = keyboard.nextFloat();
+        }
+        
+        out.println("\nTotal: " + total + "\n");
+        out.print("You entered " + tally + " numbers, for a final total of " + total);
+        
+    }
+
+}
+
---
+++ jwj/assignments/misc/GuessMyNum.java
@@ -0,0 +1,73 @@
+package misc;
+
+import static java.lang.System.out; 
+import java.util.Scanner;
+import java.util.Random;
+
+public class GuessMyNum 
+{ 
+    public static int getIntegerBetween(int lower, int upper) {
+        Scanner keyboard = new Scanner(System.in);
+        int number;
+        do {
+            number = keyboard.nextInt();
+            if (number < lower || number > upper) {
+                out.println("Oops. That number isn't between " + lower + " and " + upper + ". Try again.");
+            }
+        }
+        while (number < lower || number > upper);
+        return number;
+    }
+    
+    public static boolean hiLow(int guess, int randomNum, int count) {
+        if (guess > randomNum) {
+            out.print("My number is lower.\nGuess again:\n");
+            return true;
+        }
+        else if (guess < randomNum) {
+            out.print("My number is higher.\nGuess again:\n");
+            return true;
+        }
+        else {
+            out.println("You got it! My number was " + randomNum + ".\nIt took you " + count + " guesses.\n");
+            return false;
+        }
+        
+    }
+    
+    
+    public static void main(String[] args) 
+    { 
+        Scanner keyboard = new Scanner(System.in);
+        
+        int guess;
+        int randomNum;
+        int count = 0;
+        boolean keepPlaying = true;
+        boolean keepGuessing = true;
+        String playAgain;
+        Random random = new Random(5);
+        
+        do {
+            randomNum = random.nextInt(100);
+            out.println("I'm thinking of a number between 1 and 100. What is it?");
+            //out.println("Shhh... my number is " + randomNum);
+            
+            do {
+                guess = getIntegerBetween(1, 100);
+                count++;
+                keepGuessing = hiLow(guess, randomNum, count);
+            } while (keepGuessing);
+            out.println("Would you like to play again (y or n)?");
+            playAgain = keyboard.nextLine();
+            if (playAgain.equals("n") || playAgain.equals("N") || playAgain.equals("no") || playAgain.equals("No")) {
+                keepPlaying = false;
+            }
+            else {
+                count = 0;
+            }
+        } while (keepPlaying);
+        out.print("Good-bye!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+
+    } 
+}
