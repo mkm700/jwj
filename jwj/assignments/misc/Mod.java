dffad936d699a7576db2c7e4bbedd3568074b2fa
MK <murdaughmk@yahoo.com>
2016-10-04 20:54:48 -0500

    add mod

---
+++ jwj/assignments/misc/Mod.java
@@ -0,0 +1,18 @@
+package misc;
+
+public class Mod {
+
+	public static void main(String[] args) {
+		for (int i = -2; i < 5; i++) {
+			System.out.println(i + " % 2 = " + i % 2);
+			System.out.println("2 % " + i + " = " + 2 % i);
+			System.out.println(i + " % 3 = " + i % 3);
+			System.out.println(i + " % 4 = " + i % 4);
+			System.out.println(i + " % 5 = " + i % 5);
+		}
+		System.out.println();
+		
+
+	}
+
+}
